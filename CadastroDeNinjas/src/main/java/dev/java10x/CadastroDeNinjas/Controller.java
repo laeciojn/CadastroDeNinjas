5ea0925e547e7710e26cf2911e47fc6139f279d4
Laecio Jesus <laeciojesusn@gmail.com>
2025-12-03 13:15:09 -0300

    criado a controller

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,18 @@
+package dev.java10x.CadastroDeNinjas;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping // colocaar todas as rotas no mesmo lugar
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota.";
+    }
+
+
+}
