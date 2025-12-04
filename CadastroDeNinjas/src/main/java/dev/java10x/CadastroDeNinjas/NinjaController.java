6069a674dfe124359a1eb0a6eb643482344d714f
Laecio Jesus <laeciojesusn@gmail.com>
2025-12-04 00:03:13 -0300

    Cria uma entidade no banco de dados apartir do NinjaModel

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping // colocaar todas as rotas no mesmo lugar
-public class Controller {
+public class NinjaController {
 
     @GetMapping("/boasVindas")
     public String boasVindas(){
